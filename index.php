ca571b04b868fb91877bd4d1693203d0f7f7b658
Robert Weber <rweber@MSI-Ubuntu-24.04.3>
2025-12-20 10:09:25 -0500

    Added vba, updated code flow for examples

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css" rel="stylesheet" id="prism-theme-dark" disabled>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism.min.css" rel="stylesheet" id="prism-theme-light">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
     <style>
         body {
@@ -127,7 +128,7 @@
                 </div>
                 <div class="form-check form-switch">
                     <input class="form-check-input" type="checkbox" role="switch" id="theme-toggle">
-                    <label class="form-check-label" for="theme-toggle">Dark Mode</label>
+                    <label class="form-check-label" for="theme-toggle"><i class="bi bi-moon"></i></label>
                 </div>
             </div>
         </div>
@@ -268,9 +269,7 @@
 
                         let exampleHtml = '';
                         if (item.example) {
-                            exampleHtml = `
-                            <pre class="my-2 py-2"><code style="overflow:scroll;" class="language-${data.language}">${item.example.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</code></pre>
-                        `;
+                            exampleHtml = `<pre class="my-2 py-2"><code style="overflow:scroll;" class="language-${data.language}">${item.example.replace(/</g, "&lt;").replace(/>/g, "&gt;")}<br /><br /></code></pre>`;
                         }
 
                         const itemDescription = item.description ? `<p class="card-text">${item.description}</p>` : '';
